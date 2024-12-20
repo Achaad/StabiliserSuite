90de8c79e73d21b854ff0c190a783ea6bc5dbe28
Achaad <achaad@achaad.eu>
2024-12-20 15:56:41 +0200

    Added test case for Hadamard gate

--- tests/clifford_tests.py
+++
@@ -1,7 +0,0 @@
-from clifford_builder import clifford
-
-
-class CliffordTests:
-    def test_tableu(self):
-        t = tableu.tableu_test()
-        assert t == 3
---
+++ tests/test_clifford.py
@@ -0,0 +1,31 @@
+import numpy as np
+import qiskit
+from qiskit import QuantumCircuit
+from matplotlib import pyplot as plt
+import termtables
+from clifford_builder import clifford
+
+
+def test_generate_clifford_group():
+    assert False
+
+
+def test___apply_hadamard():
+    qc = QuantumCircuit(2)
+    tableau = np.array([[0, 0, 1, 0, 1], [1, 1, 0, 1, 0]])
+    clifford.__apply_hadamard(tableau, qc, 0)
+    expected_tableau = np.array([[1, 0, 0, 0, 1], [0, 1, 1, 1, 0]])
+    assert len(qc.data) == 1
+    assert qc.data[0].name == 'h'
+    assert np.array_equal(tableau, expected_tableau)
+    assert qc.data[0].qubits[0]._index == 0
+
+    # Perform the same with shift
+    qc = QuantumCircuit(2)
+    tableau = np.array([[0, 0, 1, 1, 1], [1, 1, 0, 0, 0]])
+    clifford.__apply_hadamard(tableau, qc, 0, shift=1)
+    expected_tableau = np.array([[1, 0, 0, 1, 1], [0, 1, 1, 0, 0]])
+    assert len(qc.data) == 1
+    assert qc.data[0].name == 'h'
+    assert np.array_equal(tableau, expected_tableau)
+    assert qc.data[0].qubits[0]._index == 1
