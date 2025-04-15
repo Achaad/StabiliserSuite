5b4923e317ca502abe73dbf2d2ae4e60daad3293
Achaad <achaad@achaad.eu>
2025-04-15 13:48:00 +0300

    Implement CNOT bridging

--- clifford_builder/main.py
+++ clifford_builder/main.py
@@ -1,18 +1,31 @@
 import time
+import circuit
 
 from matplotlib import pyplot as plt
+from qiskit import QuantumCircuit
 from qiskit.transpiler import CouplingMap
 
 from clifford_builder.clifford import sample_clifford_group
 
 if __name__ == '__main__':
+    print("Example of random clifford circuit generation")
     coupling = CouplingMap([[0, 1], [2, 3], [1, 2], [2, 4], [0, 5]])
     start = time.process_time_ns()
     qcirc = sample_clifford_group(6, True, coupling_map=coupling, max_distance=1)
     end = time.process_time_ns()
+    plt.figure()
+    plt.subplot(211)
     qcirc.draw("mpl")
-    plt.show()
     draw = time.process_time_ns()
 
     print(f"Elapsed time: {(end - start) / 1000000} ms")
     print(f"Draw time: {(draw - end) / 1000000} ms")
+
+    print()
+    print("Example of CNOT gate application")
+    qc = QuantumCircuit(7)
+    coupling = CouplingMap([[0, 1], [2, 3], [1, 2], [2, 4], [0, 5], [5, 6]])
+    circuit.cnot(qc, 1, 6, coupling_map=coupling)
+    plt.subplot(212)
+    qc.draw("mpl")
+    plt.show()
---
+++ tests/test_circuit.py
@@ -0,0 +1,48 @@
+from _pytest.python_api import raises
+from qiskit import QuantumCircuit
+from qiskit.transpiler import CouplingMap
+
+from clifford_builder import circuit
+
+
+def test_cnot():
+    # Verify that CNOT is applied correctly when no coupling map is given
+    qc = QuantumCircuit(3)
+    circuit.cnot(qc, 0, 2)
+    assert len(qc.data) == 1
+    assert qc.data[0].name == 'cx'
+    assert qc.data[0].qubits[0]._index == 0
+    assert qc.data[0].qubits[1]._index == 2
+
+    # Verify that error is raised in case of impossible operation
+    coupling_map = CouplingMap([[0, 1], [2, 3]])
+    qc = QuantumCircuit(4)
+    with raises(ValueError):
+        circuit.cnot(qc, 0, 3, coupling_map=coupling_map)
+
+    # Verify that CNOT is applied correctly with a coupling map with direct connection
+    coupling_map = CouplingMap([[0, 1], [1, 2]])
+    qc = QuantumCircuit(3)
+    circuit.cnot(qc, 1, 2, coupling_map=coupling_map)
+    assert len(qc.data) == 1
+    assert qc.data[0].name == 'cx'
+    assert qc.data[0].qubits[0]._index == 1
+    assert qc.data[0].qubits[1]._index == 2
+
+    # Verify that CNOT is applied correctly with a coupling map with indirect connection
+    coupling_map = CouplingMap([[0, 1], [2, 3], [1, 2]])
+    qc = QuantumCircuit(4)
+    circuit.cnot(qc, 0, 2, coupling_map=coupling_map)
+    assert len(qc.data) == 4
+    assert qc.data[0].name == 'cx'
+    assert qc.data[0].qubits[0]._index == 0
+    assert qc.data[0].qubits[1]._index == 1
+    assert qc.data[1].name == 'cx'
+    assert qc.data[1].qubits[0]._index == 1
+    assert qc.data[1].qubits[1]._index == 2
+    assert qc.data[2].name == 'cx'
+    assert qc.data[2].qubits[0]._index == 0
+    assert qc.data[2].qubits[1]._index == 1
+    assert qc.data[3].name == 'cx'
+    assert qc.data[3].qubits[0]._index == 1
+    assert qc.data[3].qubits[1]._index == 2
