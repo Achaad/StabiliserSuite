bd79d6c8e940870f393707757125cd5fcd284e1f
Achaad <achaad@achaad.eu>
2025-05-15 14:16:18 +0300

    Add automatic release versioning

--- release.config.cjs
+++ release.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
         [
             "@semantic-release/exec",
             {
-                "generateNotesCmd": "sed -i \"s/^version\\s*=\\s*'.*'/version = '${nextRelease.version}'/\"" +
+                "prepare": "sed -i \"s/^version\\s*=\\s*'.*'/version = '${nextRelease.version}'/\"" +
                     " pyproject.toml"
             }
         ],
